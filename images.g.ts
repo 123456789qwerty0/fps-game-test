f1432c32c2ae77a3238cc41ecc72dcf873f0e524
123456789qwerty0 <140762300+123456789qwerty0@users.noreply.github.com>
2024-01-08 20:45:55 -0500

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,45 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "mutebutton":return img`
+. . . . . . . . 2 2 2 . . . . . 
+. . . . . . . 2 2 2 2 2 . . . . 
+. . . . . . 2 2 2 2 2 2 . . . . 
+. . . . . 2 2 2 2 2 2 2 . . . . 
+2 2 2 2 2 2 2 2 2 2 2 2 f . . f 
+2 2 2 2 2 2 2 2 2 2 2 2 f . . f 
+2 2 2 2 2 2 2 2 2 2 2 2 . f f . 
+2 2 2 2 2 2 2 2 2 2 2 2 . f f . 
+2 2 2 2 2 2 2 2 2 2 2 2 . f f . 
+2 2 2 2 2 2 2 2 2 2 2 2 f . . f 
+2 2 2 2 2 2 2 2 2 2 2 2 f . . f 
+2 2 2 2 2 2 2 2 2 2 2 2 . . . . 
+2 2 2 2 2 2 2 2 2 2 2 2 . . . . 
+. . . . . 2 2 2 2 2 2 2 . . . . 
+. . . . . . 2 2 2 2 2 . . . . . 
+. . . . . . . 2 2 2 . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "song1":
+            case "theme":return hex`0078000408060100001c00010a006400f401640000040000000000000000000000000005000004f00000000400012c04000800012908000c0001250c001000012210001400011e14001800012018001c0001241c002000012720002400012a24002800012a28002c0001272c003000012430003400012034003800011d38003c0001203c004000012440004400012744004800012a48004c00012a4c005000012750005400012454005800012058005c00011d5c006000012060006400012464006800012768006c00012a6c007000012770007400012474007800012078007c00011d7c008000012080008400012484008800012788008c00012a8c009000012a90009400012794009800012498009c0001209c00a000011d`;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
